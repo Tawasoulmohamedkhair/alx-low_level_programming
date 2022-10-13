a54de4e2fe11c027ec7697ee913edf5c78bc1369
Tawasoulmohamedkhair <Tawasoulmohamedkhair@gmail.com>
2022-10-13 04:18:19 -0700

    Task 5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
 /**
- *main-ENTRY
- *Return:Always 0
+ * main-ENTRY
+ * Return:Always 0
  **/
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
-       	return (0);
+	return (0);
 }
-
