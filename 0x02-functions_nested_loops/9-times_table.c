f2d88eb6abf4de1f19ba395ddb75dceb77e787f4
Tawasoulmohamedkhair <Tawasoulmohamedkhair@gmail.com>
2022-10-23 06:39:25 -0700

    TAsk 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,7 +8,7 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		for (mult = 1; mult <= 9; mult++)
+		for (mult = 0 ; mult <= 9; mult++)
 		{
 			prot = num * mult;
 			if (mult != 0)
