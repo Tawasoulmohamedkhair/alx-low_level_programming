83a7d98e125082fb25317dccf6fc7baceeb559f4
Tawasoulmohamedkhair <Tawasoulmohamedkhair@gmail.com>
2022-10-24 13:38:16 -0700

    Task

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _atoi - convert a string to an inte
+ * @s: the string to be comverted.
+ *Return: The integer value of the comverted string.
+ **/
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int num = 0;
+	do{
+	if (*s == )
+	       	sign 
+	else if ( && *s <= '9')
+						                num = (num * 10) + (*s - '0');
+
+					            else if (num > 0)
+							                break;
+						        } while (*s++);
+
+		        return (num * sign);
+}
+
+
