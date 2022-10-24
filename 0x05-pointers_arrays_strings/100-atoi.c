e283009ec262c26f2b04c60c65fb8417c5906dec
Tawasoulmohamedkhair <Tawasoulmohamedkhair@gmail.com>
2022-10-24 13:53:59 -0700

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,9 +9,10 @@ int _atoi(char *s)
 {
 	int sign = 1;
 	unsigned int num = 0;
-	do{
+
+	do {
 	if (*s == '_')
-	       	sign *= -1;
+		sign *= -1;
 	else if (*s >= '0' && *s <= '9')
 		num = (num * 10) + (*s - '0');
 	else if (num > 0)
