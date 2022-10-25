8740ef280a8e550982a834a1bd3ad91403fbc8dd
Tawasoulmohamedkhair <Tawasoulmohamedkhair@gmail.com>
2022-10-25 14:02:45 -0700

    Task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * print_numbers - print 0 - 9
+ * only using _putchar twice
+ * Return: Always 0 (Success)
+ **/
+
+void print_numbers(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		_putchar(num++ + '0');
+
+		_putchar('\n');
+	}
+}
