deee2834b32b97f4c9c6d76851b4b14f2c0c8032
Tawasoulmohamedkhair <Tawasoulmohamedkhair@gmail.com>
2022-10-27 01:43:34 -0700

    TAsk 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,18 +7,24 @@
  * Return:char
  **/
 char *_strncat(char *dest, char *src, int n)
+
 {
 	int i, j;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	i = 0;
+
+
+	while (dest[i] != '\0')
 	{
-		continue;
+		i++;
 	}
-	for (j = 0; src[j] != '\0' && j < n; j++)
+	j = 0;
+	while (j < n && src[j] != '\0')
 	{
-	
-		dest[i + j] = src[j];
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
-	dest[i + j] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
