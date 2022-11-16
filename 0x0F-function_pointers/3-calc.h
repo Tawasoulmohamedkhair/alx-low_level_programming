d1dd3bd154e66ca57193071b89b552d7a7c3c5ac
Tawasoulmohamedkhair <Tawasoulmohamedkhair@gmail.com>
2022-11-16 05:36:47 -0800

    Task3

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,6 +1,10 @@
 #ifndef CALC_H
 #define CALC_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 int (*get_op_func(char *s))(int, int);
 int op_add(int a, int b);
 int op_sub(int a, int b);
