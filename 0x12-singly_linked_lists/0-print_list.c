20bd5d0873752da4fd2019031ee16d8ab64ced0c
Tawasoulmohamedkhair <Tawasoulmohamedkhair@gmail.com>
2022-11-24 04:13:49 -0800

    Readme && Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,7 +9,7 @@ include "lists.h"
 size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
-	
+
 	while (h)
 	{
 		if (h->str == NULL)
@@ -20,6 +20,6 @@ size_t print_list(const list_t *h)
 		nodes++;
 		h = h->next;
 	}
-	
+
 	return (nodes);
 }
